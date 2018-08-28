ab8e437fbb1eac962f83bc81d2354b4cf6e1d814
maecapozzi <maecapozzi@gmail.com>
2018-08-28 11:31:22 -0400

    Describe key signature and mode and write tests

--- src/audioAnalysis/index.js
+++ src/audioAnalysis/index.js
@@ -23,3 +23,38 @@ export const organizeDataToPassToChart = (
     }
   ]
 }
+
+export const determineKeySignature = (key, mode) => {
+  const possibleKeys = [
+    'C',
+    'C#/D♭',
+    'D',
+    'D#/E♭',
+    'E',
+    'F',
+    'F#/G♭',
+    'G',
+    'G#/A♭',
+    'A',
+    'A#/B♭',
+    'B'
+  ]
+
+  if (mode === 0) {
+    if (typeof possibleKeys[key + 9] !== 'undefined') {
+      return possibleKeys[key + 9]
+    } else {
+      return possibleKeys[key - 3]
+    }
+  } else {
+    return possibleKeys[key]
+  }
+}
+
+export const determineMode = mode => {
+  return mode === 0 ? 'Minor key' : 'Major key'
+}
+
+export const isLive = liveness => {
+  return liveness >= 0.8 ? 'Live' : 'Not live'
+}
