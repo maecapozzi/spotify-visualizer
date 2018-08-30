8e597d429f978e71f2307a00ede49804adf3e068
maecapozzi <maecapozzi@gmail.com>
2018-08-30 11:49:29 -0400

    Use staging url instead of dev

--- src/authentication/redirectToSpotify.js
+++ src/authentication/redirectToSpotify.js
@@ -3,7 +3,7 @@ export const redirectToSpotify = () => {
   const REDIRECT_URL = `${AUTH_URL}client_id=${
     process.env.REACT_APP_SPOTIFY_CLIENT_ID
   }&redirect_uri=${
-    process.env.REACT_APP_CALLBACK_URL
+    process.env.REACT_APP_CALLBACK_URL_PROD
   }&scope=user-read-private%20user-read-email&response_type=token&state=123`
   window.location = REDIRECT_URL
 }
